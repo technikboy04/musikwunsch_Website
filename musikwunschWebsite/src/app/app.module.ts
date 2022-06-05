17ef409ceb5f92d317aba92b30f264313f853aff
Helen_H <h.hoeckrich@gtmx.de>
2022-06-05 10:00:48 +0200

    first header

--- musikwunschWebsite/src/app/app.module.ts
+++ musikwunschWebsite/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
